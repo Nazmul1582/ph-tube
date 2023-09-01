95c1cc8adcab21dbd9fdc6b71d32baf4872e512b
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-01 19:57:32 +0600

    displayed all videos for each category

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const categoriesContainer = document.getElementById('categories')
+const videosContainer = document.getElementById('videos')
 
 // load all categories
 const loadCategories = async() => {
@@ -25,6 +26,25 @@ const loadVideos = async(id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const result = await res.json();
     const data = result.data;
-    console.log(data);
+    displayAllVideos(data);
 }
 
+const displayAllVideos = (videos) => {
+    videosContainer.innerText = "";
+    videos.forEach(video => {
+        console.log(video);
+        const div = document.createElement("div");
+        div.className = "card cursor-pointer border-2";
+        div.innerHTML = `
+        <figure>
+            <img src="${video.thumbnail}" alt="video" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${video.title}</h2>
+            <span>${video?.authors[0]?.profile_name}</span>
+            <span>${video?.others?.views} views</span>
+        </div>
+        `
+        videosContainer.appendChild(div)
+    })
+}

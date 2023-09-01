5edfb445073c24925979b472f07af8f14914b429
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-09-01 19:14:58 +0600

    load all videos for individual category

--- js/script.js
+++ js/script.js
@@ -12,9 +12,19 @@ const displayCategories = (categories) => {
     categories.forEach(item => {
         const button = document.createElement("button");
         button.innerText = `${item.category}`;
-        button.setAttribute('class', 'btn')        
+        button.setAttribute('class', `btn ${item.category}`)
+        button.setAttribute('onclick', `loadVideos(${item.category_id})`)        
         categoriesContainer.appendChild(button)
     })
 }
 
 loadCategories()
+
+// load all videos for individual category
+const loadVideos = async(id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const result = await res.json();
+    const data = result.data;
+    console.log(data);
+}
+
